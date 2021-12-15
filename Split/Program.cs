d5bbfae22c4407284f49462bf7053d3c91dc3488
mc0re <mnikitin0@gmail.com>
2021-12-15 20:58:57 +0100

    Transfer code from PC.

---
+++ Split/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+var input = Console.ReadLine();
+var words = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+Console.WriteLine($"Got {words.Length} words.");
+foreach (var word in words)
+{
+    Console.WriteLine(word);
+}
